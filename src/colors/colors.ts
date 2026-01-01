354f4e554b031bd322d9f0798f8c1097909928af
danushka <danmadushzxc@gmail.com>
2026-01-01 21:24:32 +0530

    feat:added theme colors

---
+++ src/colors/colors.ts
@@ -0,0 +1,23 @@
+export const Colors = {
+  light: {
+    primary: '#00BC7A',
+    secondary: '#015F97',
+    background: '#FFFFFF',
+    surface: '#F5F5F5',
+    card: '#FFFFFF',
+    text: '#111827',
+    border: '#E5E7EB',
+    error: '#EF4444',
+  },
+
+  dark: {
+    primary: '#00BC7A',
+    secondary: '#015F97',
+    background: '#070C20',
+    surface: '#1C293C',
+    card: '#1C293C',
+    text: '#F9FAFB',
+    border: '#334155',
+    error: '#F87171',
+  },
+};
